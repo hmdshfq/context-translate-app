34752b4ab455b1a4c5017b700156b138b2dc075e
Hammad <hmdshfq@outlook.com>
2022-05-04 18:29:40 +0200

    Add context provider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import UserCreate from '/src/components/UserCreate'
+import LanguageContext from '../contexts/LanguageContext'
 
 export default class App extends React.Component{
   state = {
@@ -16,9 +17,11 @@ export default class App extends React.Component{
   render() {
     return (<div className="ui container" style={this.style}>
       <span>Select a language: </span>
-      <i className="flag us" onClick={()=>this.onLanguageChange('english')}></i>
-      <i className="flag pl" onClick={()=>this.onLanguageChange('polish')}></i>
-      <UserCreate/>
+      <i className="flag us" onClick={()=>this.onLanguageChange('english')} style={{cursor: 'pointer'}}></i>
+      <i className="flag pl" onClick={()=>this.onLanguageChange('polish')} style={{cursor: 'pointer'}}></i>
+      <LanguageContext.Provider value={this.state.language}>
+        <UserCreate/>
+      </LanguageContext.Provider>
     </div>
     )
   }
